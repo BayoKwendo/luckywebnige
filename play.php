31c3e1f1b7cd8c423f744e3b536b4a6afe2296ba
Brian Kwendo <bkwendo@onfonmedia.com>
2020-10-15 15:07:10 +0300

    updates

--- play.php
+++ play.php
@@ -63,13 +63,13 @@
 
 <!-- JavaScripts -->
 <!--Start of Zendesk Chat Script-->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
 $.src="https://v2.zopim.com/?6H0NFhxG4lNqPyNkddw06yl06RG7raiv";z.t=+new Date;$.
 type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
-</script>
+</script> -->
 <!--End of Zendesk Chat Script-->
 
 <script src="js/vendors/modernizr.js"></script>
